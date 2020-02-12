1fd83df8e7ffb86d062c93fc2d79e9b22b704eb8
Elen34 <lauelena_14@hotmail>
2020-02-12 10:32:56 -0600

    Se creo una practica para calcular el radio de un circulo

---
+++ Practica4/App.js/javascript.js
@@ -0,0 +1,29 @@
+ let radio;
+ let resultado;
+
+ const constantePi=3.1416;
+
+ let button = document.getElementById('Calcularpi');
+
+ button.addEventListener('click', areacirculo);
+
+ function areacirculo(){
+
+    radio = Number(document.getElementById('radios').value);
+
+    //let resultado = constantePi * (radio*radio);
+
+    let radioelevado = Math.pow(radio,2); // Libreria Math se utiliza para hacer calculos
+    //codigo para elevar el radio al cuadrado
+
+    resultado = radioelevado*Math.PI;
+
+    //console.log() //mostar el resultado
+
+    let imprimir = document.getElementById('ImprimirResultado');
+    imprimir.innerText = resultado;
+
+ }
+
+
+ 
