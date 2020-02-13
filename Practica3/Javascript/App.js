4fd06222bdedafd3e88bc4c487e682aa6dbbebf3
Elen34 <lauelena_14@hotmail>
2020-02-12 18:39:35 -0600

    Se creo una practica para calcular el area de un triangulo

--- Practica3/Javascript/App.js
+++ Practica3/Javascript/App.js
@@ -1,20 +1,17 @@
- let base;
- let altura;
- let resultado;
+let base;
+let altura;
+let resultado;
 
+let button = document.getElementById("boton");
 
- let button=document.getElementById('boton');
+button.addEventListener("click", calcular);
 
- button.addEventListener('click', calcular);
+function calcular() {
+  base = Number(document.getElementById("bases").value);
+  altura = Number(document.getElementById("alturas").value);
 
- function calcular(){
+  resultado = (base * altura) / 2;
 
-     base = Number(document.getElementById('bases').value);
-     altura = Number(document.getElementById('alturas').value);
-
-     resultado = (base * altura) / 2;
-     
-     let ImprimirResultado = document.getElementById('ImprimirResultado'); 
-     ImprimirResultado.innerText=resultado;
-
- }
+  let ImprimirResultado = document.getElementById("ImprimirResultado");
+  ImprimirResultado.innerText = resultado;
+}
