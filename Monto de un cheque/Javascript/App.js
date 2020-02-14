748b12bb8db22ccbfa8a488cefe631e1975829e4
Elen34 <lauelena_14@hotmail>
2020-02-14 09:39:14 -0600

    Se creo una practica para calcular el cheque de un empleado

---
+++ Monto de un cheque/Javascript/App.js
@@ -0,0 +1,20 @@
+let diasfuera;
+let hotel;
+let comida;
+let cheque;
+const adicional = 100;
+
+let button = document.getElementById("Calcularmontos");
+
+button.addEventListener("click", calcularmonto);
+
+function calcularmonto() {
+  diasfuera = Number(document.getElementById("fueraciudad").value);
+  hotel = Number(document.getElementById("costohotel").value);
+  comida = Number(document.getElementById("costocomida").value);
+
+  cheque = hotel * diasfuera + comida * diasfuera + adicional;
+
+  let imprimir = document.getElementById("idimprimir");
+  imprimir.innerText = cheque;
+}
