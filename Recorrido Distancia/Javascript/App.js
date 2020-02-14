c9453f31f43e7163867509114a1bb0028f3ac62d
Elen34 <lauelena_14@hotmail>
2020-02-13 21:46:05 -0600

    Se creo una practica para calcular la distancia recorrida por un movil

---
+++ Recorrido Distancia/Javascript/App.js
@@ -0,0 +1,19 @@
+let rapidez;
+let tiempo;
+let distancia;
+
+let button = document.getElementById('CalcularDistancia');
+
+button.addEventListener('click', Distancia);
+
+function Distancia(){
+
+    rapidez = Number(document.getElementById('rapreco').value);
+    tiempo = Number(document.getElementById('tiemporec').value);
+
+    distancia = rapidez * tiempo;
+
+    let imprimir = document.getElementById('Imprimirdistancia');
+    imprimir.innerText = distancia;
+
+}
