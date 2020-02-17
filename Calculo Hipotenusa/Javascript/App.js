4e97efa33e01ea3740b76b7cc8fe6e6b97c91d80
Elen34 <lauelena_14@hotmail>
2020-02-16 21:54:25 -0600

    Se creo una practica para el calculo de la hipotenusa

--- Calculo Hipotenusa/Javascript/App.js
+++ Calculo Hipotenusa/Javascript/App.js
@@ -4,18 +4,14 @@ let resultado;
 
 let button = document.getElementById("btnCalcular");
 
-button.addEventListener('click', hipotenusa);
+button.addEventListener("click", hipotenusa);
 
 function hipotenusa() {
   lado1 = Number(document.getElementById("ladoA").value);
   lado2 = Number(document.getElementById("ladoB").value);
 
-  let elevacioncuad = Math.pow(lado1, 2);
-  let elevacioncuad2 = Math.pow(lado2, 2);
-
-  resultado = Math.sqrt(elevacioncuad + elevacioncuad2);
+  resultado = Math.sqrt(Math.pow(lado1, 2) + Math.pow(lado2, 2));
 
   let imprimir = document.getElementById("impresultado");
   imprimir.innerText = resultado;
-
 }
