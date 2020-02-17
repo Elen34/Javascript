d691aa53a5ee2fb7704873f8d69f3abcdf34e2e6
Elen34 <lauelena_14@hotmail>
2020-02-16 19:05:50 -0600

    Se creo una pagina para el pago de agua

---
+++ Pago del agua/Javascript/App.js
@@ -0,0 +1,22 @@
+let lado;
+let ancho;
+let profulitros;
+let litros;
+let resultado;
+
+let button = document.getElementById("Calculo");
+
+button.addEventListener("click", calcularlitro);
+
+function calcularlitro() {
+  lado = Number(document.getElementById("lados").value);
+  ancho = Number(document.getElementById("anchos").value);
+  profulitros = Number(document.getElementById("profundiadlit").value);
+  litros =  Number(document.getElementById('litros').value);
+
+  resultado = litros * (lado * ancho * profulitros);
+
+  let imprimir = document.getElementById("imprimirlitros");
+  imprimir.innerText = resultado;
+
+}
