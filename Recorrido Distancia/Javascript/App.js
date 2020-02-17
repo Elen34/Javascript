8f43fb7e3d11951dffccd03a17034ec92f451a0f
Elen34 <lauelena_14@hotmail>
2020-02-16 21:23:42 -0600

    Se creo una practica para el recorrido de un automovil

--- Recorrido Distancia/Javascript/App.js
+++ Recorrido Distancia/Javascript/App.js
@@ -2,18 +2,16 @@ let rapidez;
 let tiempo;
 let distancia;
 
-let button = document.getElementById('CalcularDistancia');
+let button = document.getElementById("CalcularDistancia");
 
-button.addEventListener('click', Distancia);
+button.addEventListener("click", Distancia);
 
-function Distancia(){
+function Distancia() {
+  rapidez = Number(document.getElementById("rapreco").value);
+  tiempo = Number(document.getElementById("tiemporec").value);
 
-    rapidez = Number(document.getElementById('rapreco').value);
-    tiempo = Number(document.getElementById('tiemporec').value);
+  distancia = rapidez * tiempo;
 
-    distancia = rapidez * tiempo;
-
-    let imprimir = document.getElementById('Imprimirdistancia');
-    imprimir.innerText = distancia;
-
+  let imprimir = document.getElementById("Imprimirdistancia");
+  imprimir.innerText = distancia;
 }
