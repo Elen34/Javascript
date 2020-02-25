6448ee012b37d31711d3fefd6024e74ba0754216
Elen34 <lauelena_14@hotmail>
2020-02-24 22:52:37 -0600

    Se creo una practica para verificar que regalos compras

---
+++ Regalos/Javascript/App.js
@@ -0,0 +1,36 @@
+﻿const button = document.getElementById('btncomparar');
+
+button.addEventListener('click', comparaciondeprecios);
+
+function comparaciondeprecios(){
+
+    const tipopresupuesto = document.getElementById('idpresupuesto').value;
+
+    let imprimir = document.getElementById('imprimirmejorregalo');
+
+    let divimprimir;
+
+    if(tipopresupuesto >= 5 && tipopresupuesto <=10){
+
+        divimprimir = `<div class="alert alert-success btn-block mt-3"><p>Se comprará un Tarjeta</p></div>`; 
+
+    } else if(tipopresupuesto >= 11 && tipopresupuesto <= 100){
+
+        divimprimir = `<div class="alert alert-success btn-block mt-3"><p>Se comprará unos Chocolates</p></div>`;
+
+    }  else if(tipopresupuesto >= 101 && tipopresupuesto <= 250){
+
+        divimprimir = `<div class="alert alert-success btn-block mt-3"><p>Se comprará unas Flores</p></div>`;
+
+    }else if(tipopresupuesto >= 251){
+
+        divimprimir = `<div class="alert alert-success btn-block mt-3"><p>Se comprará un Anillo</p></div>`;
+
+} else{
+
+	divimprimir = `<div class="alert alert-success btn-block mt-3"><p>No se compraria un regalo</p></div>`;       
+}
+
+imprimir.innerHTML = divimprimir; 
+
+}
