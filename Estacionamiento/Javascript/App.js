a454c2afad68939ab7d330e598443e04b7841f48
Elen34 <lauelena_14@hotmail>
2020-02-24 21:32:34 -0600

    Se creo una practica para el costo de un estacionamiento

--- Estacionamiento/Javascript/App.js
+++ Estacionamiento/Javascript/App.js
@@ -12,19 +12,19 @@ function calcularprecio() {
   if (horas <= 2) {
     let costhoras1 = horas * 5;
 
-    divImprimircosto = `<div class="alert alert-success mt-3"> <p> El costo del estacionamiento es: ${costhoras1}</p></div>`;
+    divImprimircosto = `<div class="alert alert-dark mt-3"> <p> El costo del estacionamiento es: ${costhoras1}</p></div>`;
 
   } else if (horas > 2 && horas <= 5) {
     let costhoras2 = horas * 4;
-    divImprimircosto = `<div class="alert alert-success mt-3"> <p> El costo del estacionamiento es: ${costhoras2}</p></div>`;
+    divImprimircosto = `<div class="alert alert-dark mt-3"> <p> El costo del estacionamiento es: ${costhoras2}</p></div>`;
 
   } else if (horas > 5 && horas < 10) {
     let costhoras3 = horas * 3;
-    divImprimircosto = `<div class="alert alert-success mt-3"> <p> El costo del estacionamiento es: ${costhoras3}</p></div>`;
+    divImprimircosto = `<div class="alert alert-dark mt-3"> <p> El costo del estacionamiento es: ${costhoras3}</p></div>`;
 
   } else {
     let costhoras4 = horas * 2;
-    divImprimircosto = `<div class="alert alert-success mt-3"> <p> El costo del estacionamiento es: ${costhoras4}</p></div>`;
+    divImprimircosto = `<div class="alert alert-dark mt-3"> <p> El costo del estacionamiento es: ${costhoras4}</p></div>`;
   }
 
   imprimir.innerHTML = divImprimircosto;
